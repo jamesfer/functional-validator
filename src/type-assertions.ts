ab39e138fe748967d6f65b119b3094005d2e3d7d
James Ferguson <jamesferguson497@gmail.com>
2018-09-24 20:43:11 +1000

    Fix tests

--- src/type-assertions.ts
+++ src/type-assertions.ts
@@ -1,3 +1,3 @@
 export function assertType<T>(x: T) {}
 export function returns<T>(): T { return undefined as any; }
-export type Dictionary<T> = { [k: string]: T }
+export type Dictionary<T> = { [k: string]: T };

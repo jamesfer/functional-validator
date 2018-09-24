6bd5968055efdaa1ff5a9cc66b81090f7f2245ae
James Ferguson <jamesferguson497@gmail.com>
2018-09-24 20:29:14 +1000

    Improve built modules

--- rollup.config.js
+++ rollup.config.js
@@ -5,18 +5,21 @@ import path from 'path';
 
 const module = process.env.module || 'esm';
 const target = process.env.target || 'es2015';
-
-const file = module === 'umd' ? 'umd.js' : `${module}${target.replace(/^es/, '')}.js`;
-
+const name = process.env.name || `${module}${target.replace(/^es/, '')}`;
 const dependencies = Object.keys(require('./package.json').dependencies);
 
 export default {
   input: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
-    file: path.resolve(__dirname, 'dist', file),
+    file: path.resolve(__dirname, 'dist', `${name}.js`),
     format: module,
-    name: 'fv',
     sourcemap: true,
+    // Name of global variable when using with a script tag
+    name: 'fv',
+    // Skip default export when not using esm
+    exports: module === 'esm' ? 'auto' : 'named',
+    // Skip emitting interop helper function
+    interop: false,
   },
   plugins: [
     resolve(),
@@ -26,8 +29,10 @@ export default {
       tsconfig: 'tsconfig.build.json',
     }),
   ],
+  // Mark dependencies as external when not making a umd bundle
   external: module === 'umd' ? [] : id => dependencies.includes(id) || /\/lodash-es\//.test(id),
   treeshake: {
+    // Assume all dependencies don't have side effects
     pureExternalModules: true,
   },
 };

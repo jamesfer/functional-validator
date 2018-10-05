eee88149b159ddf8ce2abd02819bc267daa0f2d6
James Ferguson <jamesferguson497@gmail.com>
2018-10-05 23:18:32 +1000

    build: minify umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
+import path from 'path';
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import path from 'path';
+import { uglify } from 'rollup-plugin-uglify';
 
 const dependencies = Object.keys(require('./package.json').dependencies);
 const configurations = [
   { module: 'umd', target: 'es5' },
-  { module: 'umd', target: 'es5' },
+  { module: 'umd', target: 'es5', minify: true },
   { module: 'esm', target: 'es5' },
   { module: 'esm', target: 'es2015' },
 ];
 
-function name(module, target) {
-  return module === 'umd' ? 'umd.js' : `${module}${target.replace(/^es/, '')}.js`;
+function name(module, target, minify) {
+  const ext = `${minify ? 'min.' : ''}js`;
+  const name = module === 'umd' ? 'umd' : `${module}${target.replace(/^es/, '')}`;
+  return `${name}.${ext}`;
 }
 
-export default configurations.map(({ module, target }) => ({
+export default configurations.map(({ module, target, minify }) => ({
   input: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
-    file: path.resolve(__dirname, 'dist', name(module, target)),
+    file: path.resolve(__dirname, 'dist', name(module, target, minify)),
     format: module,
     sourcemap: true,
     // Name of global variable when using with a script tag
@@ -35,7 +38,9 @@ export default configurations.map(({ module, target }) => ({
       target,
       tsconfig: 'tsconfig.build.json',
     }),
-  ],
+  ].concat(
+    minify ? uglify() : [],
+  ),
   // Mark dependencies as external when not making a umd bundle
   external: module === 'umd' ? [] : id => dependencies.includes(id) || /\/lodash-es\//.test(id),
   treeshake: {
